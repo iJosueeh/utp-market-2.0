f6a212cdaa1d9a88bcfcbac1492ec3b411759509
IJosueeh01 <royer.tanta27@gmail.com>
2025-10-18 22:52:21 -0500

    feat: Carrito + Producto Eliminado (temporal)

--- src/main/java/com/utpmarket/utp_market/models/dto/ItemCarrito.java
+++
@@ -1,9 +0,0 @@
-package com.utpmarket.utp_market.models.dto;
-
-import com.utpmarket.utp_market.models.entity.Producto;
-import com.utpmarket.utp_market.models.entity.Usuario;
-
-public record ItemCarrito(
-        Producto producto,
-        int cantidad,
-        Usuario usuario) {}
