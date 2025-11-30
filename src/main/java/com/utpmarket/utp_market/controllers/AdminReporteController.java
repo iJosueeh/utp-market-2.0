ccc458fbd274237b863ea3a9e4a3c3c739dad0d0
AlexanderSc21 <alexandersinte21@gmail.com>
2025-11-30 03:13:14 -0500

    Filtro de Ventas (por rango de fechas)

--- src/main/java/com/utpmarket/utp_market/controllers/AdminReporteController.java
+++ src/main/java/com/utpmarket/utp_market/controllers/AdminReporteController.java
@@ -2,14 +2,17 @@ package com.utpmarket.utp_market.controllers;
 
 import com.utpmarket.utp_market.repository.PedidoRepository;
 import com.utpmarket.utp_market.repository.VentasDiarias;
+import com.utpmarket.utp_market.services.ReporteService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
@@ -19,21 +22,29 @@ import java.util.Map;
 public class AdminReporteController {
 
     @Autowired
-    private PedidoRepository pedidoRepository;
+    private ReporteService reporteService;
 
     @GetMapping
     public String viewReportes(Model model) {
         model.addAttribute("activePage", "reportes");
-        // Lógica para Desarrollador 5
         return "admin/reportes";
     }
 
     @GetMapping("/api/ventas-semanales")
     @ResponseBody
-    public Map<String, Object> getVentasSemanales() {
-        List<VentasDiarias> ventas = pedidoRepository.findVentasDiariasUltimos30Dias();
+    public Map<String, Object> getVentas(
+            @RequestParam(required = false) String inicio,
+            @RequestParam(required = false) String fin
+    ) {
 
+        LocalDate fechaInicio = (inicio != null && !inicio.isEmpty()) ? LocalDate.parse(inicio) : null;
+        LocalDate fechaFin = (fin != null && !fin.isEmpty()) ? LocalDate.parse(fin) : null;
+
+        List<VentasDiarias> ventas = reporteService.getVentasPorRango(fechaInicio, fechaFin);
+
+        // Convertir para ChartJS
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM");
+
         List<String> labels = ventas.stream()
                 .map(v -> v.getFecha().format(formatter))
                 .toList();
@@ -42,6 +53,9 @@ public class AdminReporteController {
                 .map(VentasDiarias::getTotal)
                 .toList();
 
-        return Map.of("labels", labels, "data", data);
+        return Map.of(
+                "labels", labels,
+                "data", data
+        );
     }
 }
