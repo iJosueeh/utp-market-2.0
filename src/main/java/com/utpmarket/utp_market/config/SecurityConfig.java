765b6a66640b04cc07292a10488ccca20ed6b69e
IJosueeh01 <royer.tanta27@gmail.com>
2025-12-02 01:44:39 -0500

    refactor: change cart to use traditional forms instead of AJAX for better Thymeleaf compatibility

--- src/main/java/com/utpmarket/utp_market/config/SecurityConfig.java
+++ src/main/java/com/utpmarket/utp_market/config/SecurityConfig.java
@@ -12,11 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
         @Autowired
